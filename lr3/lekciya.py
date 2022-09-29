a95f6fca5b08f62302727311b0bd2183191097ba
Apoloniy <114357746+Apoloniy@users.noreply.github.com>
2022-09-29 22:52:56 +0300

    Create lekciya.py

---
+++ lr3/lekciya.py
@@ -0,0 +1,12 @@
+import math
+x = float(input("Введите переменную x: "));
+y = float(input("Введите переменную y: "));
+if (x>=4 and y<2):
+    q = (x + x * y)
+    print(q)
+if (x < y):
+    q = (y ** 2 + 1)
+    print(q)
+else:
+    q = (y ** 2 + 4)
+    print(q)
