0196a0bed8c2217dfdb12d84db90b6a58fb6fe81
Apoloniy <114357746+Apoloniy@users.noreply.github.com>
2022-10-10 18:45:58 +0300

    Create pr2.py

---
+++ lr4/pr2.py
@@ -0,0 +1,9 @@
+A = int(input("Введите число A: "));
+B = int(input("Введите число B: "));
+if A < B 
+    for i in range (A, B + 1);
+    print(i, end=";");
+else:
+    for k in reversed(range(B, A + 1)):
+        print(k, end=";")
+
